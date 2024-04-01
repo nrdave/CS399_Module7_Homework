d6333ac18304074e8cebcfe22a8308e17b0b40c5
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-04-01 16:58:33 -0700

    Added finding the vector for each word
    The program now finds the Word object for each input word and checks
    that all words have a vector model

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ model = Model("models/glove_short.txt")
 while True:
     words = input("Please enter a comma separated list of words: ").split(",")
 
-    if len(words) < 3:
-        break
-
+    # Remove whitespace
     words = [w.strip() for w in words]
+    modeled_words = [model.find_word(w) for w in words]
+    # Check for minimum word count and that all words are modeled
+    if len(words) < 3 or None in modeled_words:
+        break
 
     print(*words, sep=", ", end=".\n")
