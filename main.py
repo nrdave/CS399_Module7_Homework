99909bf52350f2920bd1fac07a1a60c02621340a
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-04-05 12:31:04 -0700

    Refactored to make outlier removal into its own function
    Going to try to use this to allow for streamlit UI

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from wv import Model
 from scipy.stats import zscore
 
 model = Model("models/glove_short.txt")
-while True:
-    words = input("Please enter a comma separated list of words: ").split(",")
 
+
+def remove_outliers(words: list[str]) -> list[str]:
     # Remove whitespace
     words = [w.strip() for w in words]
     modeled_words = [model.find_word(w) for w in words]
     # Check for minimum word count and that all words are modeled
     if len(words) < 3 or None in modeled_words:
-        break
+        return []
     for w in modeled_words:
         w.normalize()
 
@@ -35,4 +35,15 @@ while True:
 
     words = [w for w, z in zip(words, zscores) if abs(z) < ZSCORE_THRESHOLD]
 
-    print(*words, sep=", ", end=".\n")
+    return words
+
+
+while True:
+    words = input("Please enter a comma separated list of words: ").split(",")
+
+    filtered_words = remove_outliers(words)
+
+    if not filtered_words:
+        break
+
+    print(*filtered_words, sep=", ", end=".\n")
