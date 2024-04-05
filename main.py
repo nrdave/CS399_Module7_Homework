d2fe3157c2f910ec4d8add2d040a2ae837d2a44d
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-04-05 12:58:04 -0700

    Cleaning up some stuff
    Moved word sanitation into main - remove_outliers shouldn't care about
    it
    
    Also fixed the ordering of printing filtered words to be inside the
    try-catch for a valid input

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ model = Model("models/glove_short.txt")
 
 
 def remove_outliers(words: list[str]) -> list[str]:
-    # Remove whitespace
-    words = [w.strip() for w in words]
+    """
+    Use the loaded model to remove outliers from a list of words
+
+    This function takes in a list of words and, using a given word to vector
+    model (currently Stanford's GloVe), removes "outliers".
+    """
     modeled_words = [model.find_word(w) for w in words]
     # Check for minimum word count and that all words are modeled
     if len(words) < 3 or None in modeled_words:
@@ -22,14 +26,16 @@ def remove_outliers(words: list[str]) -> list[str]:
     for w in modeled_words:
         w.normalize()
 
-    zscores = zscore(
-        [
-            sum([word.similarity(w2)
-                for w2 in modeled_words if w2 is not word])
-            for word in modeled_words
-        ]
-    )
-    zscores = [abs(z) for z in zscores]
+    zscores = [
+        abs(z)
+        for z in zscore(
+            [
+                sum([word.similarity(w2)
+                    for w2 in modeled_words if w2 is not word])
+                for word in modeled_words
+            ]
+        )
+    ]
 
     ZSCORE_THRESHOLD = 1.2
 
@@ -39,18 +45,27 @@ def remove_outliers(words: list[str]) -> list[str]:
 
 
 def main():
+    """
+    Remove outliers from a list of words provided through the terminal
+
+    This function takes a list of words from the user typed from the running
+    terminal and removes the outlier words
+    """
     while True:
         try:
             words = input(
                 "Please enter a comma separated list of words: ").split(",")
+            # Remove whitespace
+            words = [w.strip() for w in words]
+            filtered_words = remove_outliers(words)
+            if not filtered_words:
+                break
+
+            print(*filtered_words, sep=", ", end=".\n")
+
         except EOFError:
+            print("\n")
             break
-
-        filtered_words = remove_outliers(words)
-        if not filtered_words:
-            break
-
-        print(*filtered_words, sep=", ", end=".\n")
 
 
 if __name__ == "__main__":
